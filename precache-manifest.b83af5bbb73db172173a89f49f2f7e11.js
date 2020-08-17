3ebba7db9f6394e38f4a74739964440b966120ff
akshaya-venkatesh8 <akshaya.venkatesh8@gmail.com>
2020-08-18 01:57:51 +0530

    Updates

---
+++ precache-manifest.b83af5bbb73db172173a89f49f2f7e11.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ed15d4b8c41d458cadd5ba99e195337",
+    "url": "/to-do-app/index.html"
+  },
+  {
+    "revision": "71a754e5e0d8b25e6c30",
+    "url": "/to-do-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "df5f4c3fb28ee26522df",
+    "url": "/to-do-app/static/js/2.30da3cce.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/to-do-app/static/js/2.30da3cce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "71a754e5e0d8b25e6c30",
+    "url": "/to-do-app/static/js/main.9ad6d971.chunk.js"
+  },
+  {
+    "revision": "4385e1bd226fdb470057",
+    "url": "/to-do-app/static/js/runtime-main.1f8cf036.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/to-do-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
